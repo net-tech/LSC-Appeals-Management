67d475e64edb80e298cd3aa129b68aaac9f40178
tm21cy <88517473+tm21cy@users.noreply.github.com>
2021-11-12 11:29:33 -0500

    Added public suggestions.

---
+++ commands/psuggestion.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const { blue, green, red, orange, yellow, purple, pink, gray } = require('../util/colors.js')
+module.exports = {
+  aliases: ["suggest"],
+  name: "suggestion",
+  category: "Utility",
+  description: "Sends a suggestion depending on the server you're in.",
+  cooldown: "3s",
+  guildOnly: false,
+  slash: false,
+  ownerOnly: false,
+  testOnly: false,
+  callback: async ({ client, interaction, message, args }) => {
+    const suggestion = args[0];
+    const embed = new Discord.MessageEmbed()
+        .setTitle(`New Suggestion!`)
+        .setAuthor(`<@${message.author.id}>`, message.author.avatarURL)
+        .setDescription(args[0])
+        .setColor("RANDOM")
+    if (message.guild.id === "833599402802413598") {
+        client.guilds.cache.get('833599402802413598').channels.cache.get('846683365402738698').send({ embeds: [embed] })
+    }
+    }
+  }
