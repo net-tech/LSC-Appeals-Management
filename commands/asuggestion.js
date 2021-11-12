5f5d72f3d3393748d413d31780266fdbc14b730a
tm21cy <88517473+tm21cy@users.noreply.github.com>
2021-11-12 12:40:53 -0500

    Changes to suggestions, added anon suggestions.

--- commands/asuggestion.js
+++ commands/asuggestion.js
@@ -13,14 +13,15 @@ module.exports = {
   callback: async ({ client, interaction, message, args }) => {
     const suggestion = args.slice(0).join(' ');
     const embed = new Discord.MessageEmbed()
-        .setTitle(`New Suggestion!`)
-        .setAuthor(message.author.username, message.author.user.avatarURL)
+        .setTitle(`New Anonymous Suggestion!`)
         .setDescription(args[0])
         .setColor("RANDOM")
         .setFooter("Please review the above suggestion, and reply using the suggest-reply command!")
     if (message.guild.id === "833599402802413598") {
         client.guilds.cache.get('833599402802413598').channels.cache.get('846683365402738698').send({ embeds: [embed] })
-        message.reply("Your suggestion has been sent for review!")
+        message.delete(1)
+        client.guids.cache.get('833599402802413598').channels.cache.get('898949881127927828').delete(1)
+        message.channel.send("Your anonymous suggestion has been sent!")
     }
     }
   }
